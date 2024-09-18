72cd3a05eebec6e12790f0fbd619cac8e1f98249
Aman Kumar <37516866+ak3255@users.noreply.github.com>
2024-09-18 18:06:12 +0530

    Initial commit

---
+++ lib/constraints/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color tbRed = const Color(0xFFDA4040);
+
+Color tbBlue = const Color(0xFF5F52EE);
+
+Color tbBlack = const Color(0xFF3A3A3A);
+
+Color tbGrey = const Color(0xFF717171);
+
+Color tbBGColor = const Color(0xFFEEEFF5);
